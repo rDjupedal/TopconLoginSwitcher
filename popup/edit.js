bf1d8fe02c842fa79967345a19c338b74758e544
Rasmus Djupedal <radj2000@student.miun.se>
2023-04-26 15:15:43 +0200

    Version 1.3 working

---
+++ popup/edit.js
@@ -0,0 +1,88 @@
+console.log("edit.js loaded");
+const saveBtn = document.getElementById("saveBtn");
+const clearBtn = document.getElementById("clearBtn");
+const textBox = document.getElementById("editText");
+const defaultBtn = document.getElementById("defaultBtn");
+const loadServerBtn = document.getElementById("fromServerBtn");
+const statusDiv = document.getElementById("statusDiv");
+
+saveBtn.addEventListener("click", saveSettings);
+clearBtn.addEventListener("click", clearData);
+defaultBtn.addEventListener("click", defaultSettings);
+loadServerBtn.addEventListener("click", loadFromServer);
+
+document.addEventListener("DOMContentLoaded", loadSettings);
+
+function clearData() {
+    browser.storage.sync.clear();
+    setStatus("All data for this extension have been cleared");
+    textBox.value = "";
+}
+
+function setStatus(status) {
+    statusDiv.innerText = status;
+}
+
+async function loadFromServer() {
+    setStatus("Loading from server..");
+    const url = "http://192.168.10.61/sl2_logins.json";
+
+    fetch(url, {cache : "reload"})
+        .then((response) => { 
+            if (response.ok) {
+                response.json().then((data) => {
+                    console.log(data);
+                    textBox.value = JSON.stringify(data);
+                    saveSettings();
+                    setStatus("Successfully retrieved " + data.length + " customers from the server");
+                });
+            }
+
+            if (response.status !== 200) setStatus("Error getting customers from server..");
+        })
+            .catch((err) => setStatus("Error getting customers from server.."));
+}
+
+
+function defaultSettings() {
+    console.log("setting default..");
+
+    const testCustomer = {
+        "customer" : "Ncgeo Support", 
+        "username" : "customer1@gmail.se",
+        "password" : "123"
+    }
+
+    const testCustomer2 = {
+        "customer" : "Ncgeo test2", 
+        "username" : "customer2@shitmail.se",
+        "password" : "456"
+    }
+
+    let customers = [];
+    customers.push(testCustomer);
+    customers.push(testCustomer2);
+
+    textBox.value = JSON.stringify(customers);
+
+    //saveSettings();    
+}
+
+function loadSettings() {
+    console.log("loading settings..");
+
+    browser.storage.sync.get("customers")
+    .then((customers) => {
+        //console.log(customers), (error) => console.log(error)
+        //textBox.value = JSON.stringify(customers.customers, null, null);
+        if (customers.customers) textBox.value = customers.customers;
+    });
+
+}
+
+function saveSettings() {
+    
+    let customers = textBox.value;
+    browser.storage.sync.set( {"customers" : customers} );
+    setStatus("Customers saved..");    
+}
---
+++ popup/login_menu.js
@@ -0,0 +1,254 @@
+const RECENT_NUM = 5;
+
+console.log("login_menu loaded..");
+// Uncomment temp to clear saved data
+//browser.storage.sync.clear();
+
+const logOutBtn = document.getElementById("logOutBtn");
+logOutBtn.addEventListener("click", logOut);
+
+const filterTextBox = document.getElementById("textFilter");
+filterTextBox.addEventListener("input", e => {
+    customerTable.innerHTML = "";
+    updateList(filterTextBox.value);    
+});
+
+// Make the filter text box active default
+filterTextBox.focus();
+
+// Load customers from storage
+var customers = [];
+readLogins().then((readCustomers) => {
+    //customers = readCustomers;
+
+    // Sort the customers alphabetically
+    customers = readCustomers.sort((a, b) => {
+        return a.customer.localeCompare(b.customer);
+    })
+
+    // Clear the Table
+    customerTable.innerHTML = "";
+    updateList();
+});
+
+// Load recent customers from storage
+var recent = new Array(RECENT_NUM);
+readRecent().then((recentC) => {
+    //if (recentC) this.recent = JSON.parse(recentC.recent);
+    if (recentC) data = JSON.parse(recentC.recent);
+
+    for (let i = 0; i < RECENT_NUM; i++) {
+        this.recent[i] = data[i];
+    }
+
+    recentTable.innerHTML = "";
+    updateRecent();
+})
+
+async function readRecent() {
+    let recent = await browser.storage.sync.get("recent")
+        .then((r) => {
+            //console.log("read recent from storage: " + JSON.stringify(r));
+            if (r) return r;
+        })
+    return recent;
+}
+
+function updateRecent() {
+    //console.log("update recent called, recent: " + JSON.stringify(recent));
+
+    let recentTable = document.getElementById("recentTable");
+    recentTable.innerHTML = "";
+    for (let i = 0; i < recent.length; i++) {
+
+        if (!recent[i]) continue;
+        let a = document.createElement("a")
+        let link = document.createTextNode(recent[i].customer);
+        a.appendChild(link);
+
+        a.title = recent[i].customer;
+        a.addEventListener("click", e => { login(recent[i]); })
+        
+        let col = document.createElement("th");
+        col.appendChild(a);
+        let row = document.createElement("tr");
+        row.appendChild(col);
+        //customerTable.prepend(row);
+        recentTable.appendChild(row);
+    }
+    
+}
+
+function addToRecent(newEntry) {
+
+    let moveUntilIndex = recent.length - 1;
+
+    // Check if already exists
+    for (i = 0; i < RECENT_NUM; i++) {
+        if (recent[i] && recent[i].username === newEntry.username) {
+            moveUntilIndex = i;
+        }
+    }
+
+    //Move every entry down one step
+    for (i = moveUntilIndex; i > 0; i--) {
+        recent[i] = recent[i - 1];
+    }
+
+    // Add the new entry to the top of the list
+    recent[0] = newEntry;
+    browser.storage.sync.set( {"recent" : JSON.stringify(recent)} );
+
+}
+
+async function readLogins() {
+
+    let customers = await browser.storage.sync.get("customers")
+        .then((customers) => {
+            return customers.customers;
+        });
+        
+    return JSON.parse(customers);
+}
+
+// Populate a list of customers links
+async function updateList(filter) {
+
+    // If no filter is used show recent logins
+    if (!filter) updateRecent()
+    else recentTable.innerHTML = "";
+
+    customerTable = document.getElementById("customerTable");
+
+    for (let customer of customers) {
+        //console.log("creating: " + JSON.stringify(customer));
+                
+        if (filter && !customer.customer.toLowerCase().includes(filter.toLowerCase())) { continue; }
+
+        let a = document.createElement("a")
+        let link = document.createTextNode(customer.customer);
+        a.appendChild(link);
+
+        a.title = customer.customer;
+        a.addEventListener("click", e => { login(customer); })
+        
+        let col = document.createElement("th");
+        col.appendChild(a);
+        let row = document.createElement("tr");
+        row.appendChild(col);
+        customerTable.appendChild(row);  
+    }
+
+}
+
+async function login(customer) {
+
+    addToRecent(customer);
+    updateRecent();
+
+    await logOut();
+    closeTab();
+    
+    console.log("logging in..")
+    
+    browser.tabs.create({});
+
+    const cred = {
+        username : customer.username,
+        password : customer.password
+    }
+ 
+    // Get flow id
+    let flowId = await getFlowId();
+    if (!flowId) window.alert("Could not get flow id!");
+
+    // Send credentials
+    const postUrl = "https://id.auth.topcon.com/flows/" + flowId;
+
+    fetch(postUrl, {
+        method: "POST",
+        body: JSON.stringify(cred),
+        headers: {"Content-Type" : "application/vnd.pingidentity.usernamePassword.check+json"}
+    })
+    .then((data) => {
+        console.log("Login status: " + data.statusText);
+        if (data.status != 200) {
+            const sDiv = document.getElementById("statusDiv")            ;
+            sDiv.innerHTML = "Error logging in as " + customer.username + "!";
+            return;
+        }
+        
+        return data.json();
+    })
+    .then((json) => {
+        //console.log("finally: " + JSON.stringify(json))
+        let redirectUrl = json.resumeUrl;
+        console.log(redirectUrl);
+        
+        // Get the current tab and load the redirect page
+        browser.tabs.query({ currentWindow: true, highlighted : true }).then((x) => 
+        {
+            const tabId = x[0].id;
+            browser.tabs.update(tabId, {url : redirectUrl} );
+            window.close();
+        })
+    });
+
+}
+
+async function getFlowId() {
+    const url = "https://sitelink.topcon.com/login";
+    let data = await window.fetch(url);
+    return data.url.split("flowId=")[1];
+}
+
+async function logOut() {
+    console.log("logging out..")
+
+    // REMOVE ALL TOPCON RELATED COOKIES
+    clearCookies("topcon.com");
+    clearCookies("topconpositioning.com");
+}
+
+async function closeTab() {
+    browser.tabs.query({ currentWindow: true, highlighted : true }).then((x) => 
+        {
+            console.log(x[0]);
+            browser.tabs.remove(x[0].id);
+        })
+}
+
+async function clearCookies(domain) {
+
+    // FIND COOKIES
+    let cookieList = [];
+    browser.cookies.getAll( {domain: domain} )
+    .then(cookies => {
+        if (cookies.length > 0) {
+            console.log("found " + cookies.length + " cookies");   
+            
+            for (let cookie of cookies) {
+                let c = { 
+                    name : cookie.name,
+                    url  : "https://" + cookie.domain + cookie.path 
+                }
+
+                console.log(c);
+                cookieList.push(c);
+            }
+        }
+
+        else console.log("no cookie found for domain " + domain);
+
+
+        // REMOVE COOKIES
+        console.log("Removing " + cookieList.length + " cookies..");
+        for (let removeCookie of cookieList) {
+
+            browser.cookies.remove(removeCookie)
+                .then((result) => console.log("Removed " + removeCookie.url));
+        }
+    })
+
+
+}
